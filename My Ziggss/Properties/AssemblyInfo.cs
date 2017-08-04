e7babe633949aa9dbe3f4b00a49f3e1f87d3696f
cashlord <seymenaslan333@gmail.com>
2017-08-04 14:22:45 +0300

    Update
    a

--- My Ziggss/Properties/AssemblyInfo.cs
+++ My Ziggss/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FrOnDaL Lux")]
+[assembly: AssemblyTitle("My Ziggss")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FrOnDaL Lux")]
+[assembly: AssemblyProduct("My Ziggss")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
